9e21855c6f90ef4990efa7512540f99bcf2d2fdc
NorIslam04 <islamaoudi18@gmail.com>
2025-07-27 17:25:24 +0100

    usestat()

--- components/Product/Product.jsx
+++ components/Product/Product.jsx
@@ -1,16 +1,38 @@
-// src/components/ProductPage.jsx
-import styles from "./Product.module.css"; // Using CSS Modules for scoped styles
+import { useState } from "react";
+import styles from "./Product.module.css";
 import ProductCart from "./ProductCart";
 
 const Product = ({ products }) => {
+  // State to control the visibility of products
+  // This is useful if you want to display products on a button click
+  const [showProducts, setShowProducts] = useState(false);
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Product List</h2>
-      <div className={styles.grid}>
-        {products.map((product, index) => (
-          <ProductCart product={product} key={index} />
-        ))}
-      </div>
+      <button
+        type="button"
+        onClick={() => setShowProducts(true)}
+        className={styles.btn}
+      >
+        Display Products
+      </button>
+
+      <button
+        type="button"
+        onClick={() => setShowProducts(false)}
+        className={styles.btn}
+      >
+        Hide Products
+      </button>
+
+      {showProducts && (
+        <div className={styles.grid}>
+          {products.map((product, index) => (
+            <ProductCart product={product} key={index} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
