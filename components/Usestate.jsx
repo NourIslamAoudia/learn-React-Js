173041ef37420c99ff069f60538ca10a9597c2f6
NorIslam04 <islamaoudi18@gmail.com>
2025-07-28 19:50:48 +0100

    useState-3

--- components/Usestate.jsx
+++ components/Usestate.jsx
@@ -12,30 +12,38 @@ const Usestate = () => {
         Email:
         <input
           type="email"
-          value={userInfo.email}
-          onChange={(e) => setUser({ ...userInfo, email: e.target.value })} //e.target.value: to get the value of the input field
+          value={userInfo.email} //controlled input
+          onChange={(e) => setUser({ ...userInfo, email: e.target.value })}
         />
       </label>
       <label>
         Password:
         <input
           type="password"
-          value={userInfo.password}
-          onChange={(e) => setUser({ ...userInfo, password: e.target.value })} //e.target.value: to get the value of the input field
+          value={userInfo.password} //controlled input
+          onChange={(e) => setUser({ ...userInfo, password: e.target.value })}
         />
       </label>
       <div>email: {userInfo.email}</div>
       <div>password: {userInfo.password}</div>
       <div>isLoggedIn: {userInfo.isLoggedIn ? "Yes" : "No"}</div>
-      <button onClick={() => setUser({ ...userInfo, isLoggedIn: true })}>
+      <button
+        onClick={(e) =>
+          setUser({
+            email: userInfo.email,
+            password: userInfo.password,
+            isLoggedIn: true,
+          })
+        }
+      >
         Log In
       </button>
-      <button onClick={() => setUser({ ...userInfo, isLoggedIn: false })}>
+      <button
+        onClick={() => setUser({ email: "", password: "", isLoggedIn: false })}
+      >
         Log Out
       </button>
-      <button onClick={() => setUser({ ...userInfo, email: "", password: "" })}>
-        Reset
-      </button>
+      <span>userInfo: {JSON.stringify(userInfo)}</span>
     </>
   );
 };
