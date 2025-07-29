9d3e6a077631d1cd76417343c2f6216e927f8976
NorIslam04 <islamaoudi18@gmail.com>
2025-07-29 13:26:06 +0100

    useEffect

--- components/Axios.jsx
+++ components/Axios.jsx
@@ -2,19 +2,40 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Axios = () => {
-  axios
-    .get("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => {
-      const data = response.data;
-      console.log("Data fetched successfully:", data);
-    })
-    .catch((error) => {
+  const [fetchedData, setFetchedData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null); // facultatif pour gérer les erreurs
+
+  const getData = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setFetchedData(response.data);
+      setIsLoading(false);
+      console.log("Data fetched successfully:", response.data);
+    } catch (error) {
+      setError("Erreur lors du chargement");
+      setIsLoading(false);
       console.error("Error fetching data:", error);
-    });
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <h1>Axios Example</h1>
-      <ul></ul>
+      <p>Data fetched from the API will be logged in the console.</p>
+      {isLoading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <ul>
+        {!isLoading &&
+          !error &&
+          fetchedData.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+      </ul>
     </div>
   );
 };
