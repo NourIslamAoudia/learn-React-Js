fbe020d955b91e1f396e72726becbbbd5f176653
NorIslam04 <islamaoudi18@gmail.com>
2025-07-31 15:16:33 +0100

    add product to static data

--- data/product.js
+++ data/product.js
@@ -8,7 +8,7 @@ const Products = [
     rate: 3,
     description: "A high-quality wireless mouse with ergonomic design.",
     image:
-      "https://www.dusanholovej.com/files/layout/img/blog/TUTORIAL-Shampoo-and-Shower-Gel.jpg",
+      "https://www.tpstech.in/cdn/shop/products/HP_220_Silent_Wireless_Mouse_from_tpstech.in-main3.jpg?v=1657774713.jpg",
   },
   {
     id: 2,
@@ -19,7 +19,7 @@ const Products = [
     description:
       "Noise-cancelling Bluetooth headphones with long battery life.",
     image:
-      "https://www.dusanholovej.com/files/layout/img/blog/TUTORIAL-Shampoo-and-Shower-Gel.jpg",
+      "https://media.tatacroma.com/Croma%20Assets/Entertainment/Headphones%20and%20Earphones/Images/308673_jxaozj.png",
   },
   {
     id: 3,
@@ -29,7 +29,7 @@ const Products = [
     rate: 5,
     description: "Non-slip yoga mat for comfortable workouts.",
     image:
-      "https://www.dusanholovej.com/files/layout/img/blog/TUTORIAL-Shampoo-and-Shower-Gel.jpg",
+      "https://sppartos.com/cdn/shop/files/31VX-aIlgWL.jpg?v=1702469142.jpg",
   },
   {
     id: 4,
@@ -39,7 +39,7 @@ const Products = [
     rate: 1,
     description: "Durable stainless steel water bottle with vacuum insulation.",
     image:
-      "https://www.dusanholovej.com/files/layout/img/blog/TUTORIAL-Shampoo-and-Shower-Gel.jpg",
+      "https://yinyogamats.com/cdn/shop/products/insulated-stainless-steel-water-bottle-750ml-842761.jpg?v=1693962854.jpg",
   },
   {
     id: 5,
@@ -49,7 +49,37 @@ const Products = [
     rate: 5,
     description: "Adjustable desk lamp with LED lighting.",
     image:
-      "https://www.dusanholovej.com/files/layout/img/blog/TUTORIAL-Shampoo-and-Shower-Gel.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK3zEsKKVl3yfQiaagl8QoV65KeLG_st9Qzw&s.jpg",
+  },
+  {
+    id: 6,
+    title: "Gaming Keyboard",
+    price: 35.99,
+    categorie: "Electronics",
+    rate: 4,
+    description: "Mechanical gaming keyboard with RGB lighting.",
+    image:
+      "https://www.prohavit.com/cdn/shop/files/KB890L-1.jpg?v=1716456096.jpg",
+  },
+  {
+    id: 7,
+    title: "Adjustable Dumbbells",
+    price: 89.99,
+    categorie: "Fitness",
+    rate: 5,
+    description: "Adjustable dumbbells for strength training at home.",
+    image:
+      "https://corehomefitness.com/cdn/shop/products/Dumbells-setoftwo-red-860974.png?v=1723836012.jpg",
+  },
+  {
+    id: 8,
+    title: "Mini Air Purifier",
+    price: 29.99,
+    categorie: "Home & Office",
+    rate: 4,
+    description: "Compact air purifier with HEPA filter for clean air.",
+    image:
+      "https://airpurifiers.com/wp-content/uploads/2024/04/Pure-Enrichment-Mini-Air-Purifier.jpg",
   },
 ];
 
